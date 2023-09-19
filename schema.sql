54f7ce3387b7dc853b3cecc686b85dcbae8b4a88
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-09-19 19:37:08 -0300

    create database, info, and queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
-/* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE public.vet_clinic
+(
+    "ID" integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 ),
+    name text NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg numeric NOT NULL,
+    PRIMARY KEY ("ID")
 );
+
+ALTER TABLE IF EXISTS public.vet_clinic
+    OWNER to postgres;
