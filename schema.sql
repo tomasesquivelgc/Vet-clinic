97bcbb3de8b48a60d0a58a82c7b47ea9e3a99563
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-09-21 16:31:06 -0300

    create new tables and add foreign keys

--- schema.sql
+++ schema.sql
@@ -16,3 +16,51 @@ ALTER TABLE IF EXISTS public.vet_clinic
 
 ALTER TABLE IF EXISTS public.animals
     ADD COLUMN species text;
+
+-- create owner table
+CREATE TABLE public.owners
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 ),
+    "full-name" text NOT NULL,
+    age integer NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.owners
+    OWNER to postgres;
+
+-- create species table
+CREATE TABLE public.species
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 ),
+    name text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.species
+    OWNER to postgres;
+
+-- delete species column from animals table
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- create a new table called species_id
+ALTER TABLE IF EXISTS public.animals
+    ADD COLUMN species_id integer;
+
+-- add foreign key to species_id
+ALTER TABLE public.animals
+ADD CONSTRAINT fk_species_id
+    FOREIGN KEY (species_id)
+    REFERENCES public.species (id);
+
+-- create a new column called owner_id
+ALTER TABLE IF EXISTS public.animals
+    ADD COLUMN owner_id integer;
+
+-- add foreign key to owner_id
+ALTER TABLE public.animals
+ADD CONSTRAINT fk_owner_id
+    FOREIGN KEY (owner_id)
+    REFERENCES public.owners (id);
+    
